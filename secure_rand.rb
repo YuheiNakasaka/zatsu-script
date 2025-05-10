27050dc19c6b90c4fcf0db2d280970062d5efa3f
razokulover <yuhei.nakasaka@gmail.com>
2025-05-10 15:45:11 +0900

    hoge

--- secure_rand.rb
+++ secure_rand.rb
@@ -3,9 +3,9 @@ require 'openssl'
 len = gets.chomp.to_i
 
 def generate_secure_password(length = 16)
-  chars = ('a'..'z').to_a + 
-          ('A'..'Z').to_a + 
-          ('0'..'9').to_a + 
+  chars = ('a'..'z').to_a +
+          ('A'..'Z').to_a +
+          ('0'..'9').to_a +
           ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+']
   password = Array.new(length) { chars[OpenSSL::Random.random_bytes(1).unpack('C').first % chars.size] }.join
   password[rand(length)] = ['!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '_', '+'].sample
@@ -14,4 +14,3 @@ def generate_secure_password(length = 16)
 end
 
 puts generate_secure_password(32)
-
